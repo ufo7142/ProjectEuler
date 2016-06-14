22e85ee2c47f95b9ec06806f1c6bcee199be4bc4
lsgl <ufo7142@naver.com>
2016-06-14 11:53:27 +0900

    add problem10

---
+++ EulerProject/src/problem10/SummationOfPrimes.java
@@ -0,0 +1,41 @@
+package problem10;
+
+public class SummationOfPrimes {
+
+	   public static void main(String[] args) {
+					
+	        long number = 3;
+	        long sum = 2;
+	        long limit = 2000000;
+	        
+	        while(number <= limit) {
+	            if(isPrime(number)) {
+	                sum += number;
+	            }
+	            number+=2;
+	        }
+	        
+	        System.out.println(sum);
+	    
+	    }
+	    
+	    public static boolean isPrime(long number) {
+
+	        if(number % 2 == 0) {
+	            return false;
+	        }
+	        
+	        long i = 3;
+	        
+	        int range = (int)(Math.sqrt(number)) + 1;
+	        
+	        while(i < range) {
+	            if(number % i == 0) {
+	                return false;
+	            }
+	            i++;
+	        }
+	        return true;
+	    }
+	 
+	}
